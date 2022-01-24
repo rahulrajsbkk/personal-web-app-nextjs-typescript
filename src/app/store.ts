149af0b9a066691882843a3631f4dba7b6279e09
rahulrajsbkk <rahulrajsbkk@gmail.com>
2022-01-24 14:53:57 +0530

    Contact Form State Moved To Redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import contactFormReducer from "./contactFormSlice";
+
 export function makeStore() {
   return configureStore({
-    reducer: {},
+    reducer: { message: contactFormReducer },
   });
 }
 
--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -1,72 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
-import axios from "axios";
-import { toast } from "react-toastify";
+
 import classNames from "./ContactSection.module.scss";
+import ContactForm from "../ContactForm";
 
 function ContactSection(): JSX.Element {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-
-  const formSubmit = () => {
-    let formData = new FormData();
-    formData.append("entry.1534649597", name);
-    formData.append("entry.1564222794", email);
-    formData.append("entry.1034811362", subject);
-    formData.append("entry.1182816775", message);
-    const config = {
-      headers: { "content-type": "multipart/form-data" },
-    };
-    axios
-      .post(
-        "https://docs.google.com/forms/u/3/d/e/1FAIpQLSeQnSMoNuU80uKBYG-7wYyeAnSRQsQVukwxJKX0f-M6vS1oQg/formResponse",
-        formData,
-        config
-      )
-      .then((response) => {})
-      .catch((error) => {})
-      .finally(() => {
-        toast.success("Message Send!");
-        setTimeout(() => {
-          setName("");
-          setEmail("");
-          setSubject("");
-          setMessage("");
-        }, 400);
-      });
-  };
-
-  const validate = (onValidate) => {
-    switch (false) {
-      case Boolean(name):
-        toast.error("Name is Required!");
-        break;
-      case Boolean(email):
-        toast.error("Email is Required!");
-        break;
-      case Boolean(
-        email
-          .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          )
-      ):
-        toast.error("Invalid Mail Id!");
-        break;
-      case Boolean(subject):
-        toast.error("Subject is Required!");
-        break;
-      case Boolean(message):
-        toast.error("Message is Required!");
-        break;
-      default:
-        onValidate();
-        break;
-    }
-  };
-
   return (
     <section className={classNames.ContactSection}>
       <div className={classNames.title}>
@@ -135,59 +73,7 @@ function ContactSection(): JSX.Element {
             </div>
           </div>
         </div>
-
-        <form
-          className={classNames.contactForm}
-          onSubmit={(e) => {
-            e.preventDefault();
-            validate(formSubmit);
-          }}
-        >
-          <input
-            type="text"
-            className={classNames.inpHalf}
-            placeholder="YOUR NAME"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            className={classNames.inpHalf}
-            placeholder="YOUR EMAIL"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="text"
-            className={classNames.inpFull}
-            placeholder="SUBJECT"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-          />
-          <textarea
-            className={classNames.inpTextArea}
-            placeholder="YOUR MESSAGE"
-            rows={7}
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-          <label className={`link ${classNames.button}`}>
-            <input
-              type="submit"
-              value="SEND MESSAGE"
-              className={classNames.btnText}
-            />
-            <div className={classNames.btnIcon}>
-              <Image
-                src="/assets/icons/download.png"
-                alt=""
-                width={256}
-                height={256}
-                layout="responsive"
-              />
-            </div>
-          </label>
-        </form>
+        <ContactForm />
       </div>
     </section>
   );
